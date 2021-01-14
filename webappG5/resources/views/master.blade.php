46625bc453d392808c728df0254621f39b441743
danialafiqazlan <d.afiq0301@gmail.com>
2021-01-14 18:06:01 +0800

    Added Controller and Master View

---
+++ webappG5/resources/views/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Inventory Management System</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
